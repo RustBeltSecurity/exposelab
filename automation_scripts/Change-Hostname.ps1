4c3c0d5b0cb5f3453f9382d1bb064924e671bef7
Justin Forbes <64507560+justinforbes@users.noreply.github.com>
2024-04-27 21:31:33 -0400

    install .net 4.8 during hostname change

--- automation_scripts/Change-Hostname.ps1
+++ automation_scripts/Change-Hostname.ps1
@@ -8,3 +8,19 @@ Write-Host -ForegroundColor Green "Changing_hostname"
 $pass=ConvertTo-SecureString $password -AsPlainText -Force
 $cred=New-Object System.Management.Automation.PSCredential ($user, $pass)
 Rename-Computer -NewName $hostname -DomainCredential $cred
+
+$myURL="https://go.microsoft.com/fwlink/?linkid=2088631"
+$myOutFile="C:\temp\dotnet.4.8.exe"
+        
+    if ( !(test-path -path $myOutFile)  ) {
+        New-Item -Path 'c:\temp' -ItemType Directory
+        $response = Invoke-WebRequest $myURL -UseBasicParsing
+        [IO.File]::WriteAllBytes($myOutFile, $response.Content)
+    }
+
+
+    if ( (test-path -path $myOutFile)  ) {
+       Write-Host "Installing 4.8"
+    Start-Process C:\temp\dotnet.4.8.exe -ArgumentList "/q /norestart /log c:\temp\" -Wait #Fire a new process to install silently
+    Write-Host "Installed 4.8 $LASTEXITCODE"
+}
