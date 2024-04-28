9420e1c7071d935699667b744a868f4d7f8437ec
Justin Forbes <64507560+justinforbes@users.noreply.github.com>
2024-04-28 00:23:54 -0400

    changed to offline .net install and added time sync

--- automation_scripts/Change-Hostname.ps1
+++ automation_scripts/Change-Hostname.ps1
@@ -9,18 +9,10 @@ $pass=ConvertTo-SecureString $password -AsPlainText -Force
 $cred=New-Object System.Management.Automation.PSCredential ($user, $pass)
 Rename-Computer -NewName $hostname -DomainCredential $cred
 
-$myURL="https://go.microsoft.com/fwlink/?linkid=2088631"
-$myOutFile="C:\temp\dotnet.4.8.exe"
-        
-    if ( !(test-path -path $myOutFile)  ) {
-        New-Item -Path 'c:\temp' -ItemType Directory
-        $response = Invoke-WebRequest $myURL -UseBasicParsing
-        [IO.File]::WriteAllBytes($myOutFile, $response.Content)
-    }
+# syncing time
+w32tm /resync /server:time.nist.gov
 
-
-    if ( (test-path -path $myOutFile)  ) {
-       Write-Host "Installing 4.8"
-    Start-Process C:\temp\dotnet.4.8.exe -ArgumentList "/q /norestart /log c:\temp\" -Wait #Fire a new process to install silently
-    Write-Host "Installed 4.8 $LASTEXITCODE"
-}
+# install dot net 4.8
+cp C:\vagrant\CoolScripts\ndp48-x86-x64-allos-enu.exe C:\temp
+Start-Process C:\temp\ndp48-x86-x64-allos-enu.exe -ArgumentList "/q /norestart /log c:\temp\" -Wait #Fire a new process to install silently
+Write-Host "Installed 4.8 $LASTEXITCODE"
